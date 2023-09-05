ae6c8b0964d0aef7c3be7a29bb968bf5c00d3f86
MatiasDonati <matod87@hotmail.com>
2023-09-05 19:13:53 -0300

    guia completada ok

--- guia_ejercicios.py
+++ guia_ejercicios.py
@@ -123,3 +123,114 @@
 #     tipo = "descuento"
 
 # print(f"En {estacion} en la ciudad de {localidad} hay un {tipo} de {abs(porcentaje)}% \n Precio final por estadia:{precio_final}")
+
+# # Ejercicio 6:
+# # Utilizar For
+# # Dada la siguiente lista:
+# # [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
+# # mostrar el mayor.
+
+# numeros = [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
+# mayor = None
+
+# for numero in numeros:
+#     if mayor == None or numero > mayor:
+#         mayor = numero
+
+# print(mayor)
+
+# # Ejercicio 7:
+# # Dada la siguiente lista:
+# # [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
+# # mostrar solo los números pares.
+
+# numeros = [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
+# pares = []
+
+# for numero in numeros:
+#     if numero % 2 == 0:
+#         pares.append(numero)
+
+# print(pares)
+
+# # Ejercicio 8:
+# # Dada la siguiente lista:
+# # [82, 3, 49, 38, 94, 85, 95, 92, 64, 8, 75, 37, 97, 45, 12, 64, 48, 78, 29, 58]
+# # mostrar el número repetido
+
+# numeros = [82, 3, 49, 38, 94, 85, 97, 92, 64, 8, 75, 37, 67, 45, 12, 55, 48, 78, 29, 58]
+
+# VER COMO haCER !!!
+# VER COMO haCER !!!
+# VER COMO haCER !!!
+# VER COMO haCER !!!
+
+
+# # Ejercicio 9:
+# # Dadas las siguientes listas:
+# # edades = [25,36,18,23,45]
+# # nombre = ["Juan","Ana","Sol","Mario","Sonia"]
+# # y considerando que la posición en la lista corresponde a la misma persona,
+# # mostar el nombre de la persona más joven
+
+# edades = [25,36,18,23,45]
+# nombre = ["Juan","Ana","Sol","Mario","Sonia"]
+# edad_menor = None
+
+# for indice in range(len(edades)):
+#     if edad_menor == None or edades[indice] < edad_menor:
+#         edad_menor = edades[indice]
+#         nombre_menor = nombre[indice]
+
+# print(f"La persona mas joven es {nombre_menor}")
+
+# Ejercicio 10:
+# Pedir al usuario que ingrese los datos de 5 alumnos y guardarlos en sus
+# respectivas listas. Validar el ingreso de datos según su criterio.
+# Datos:
+# nombre, sexo (f/m), nota (validar).
+# Una vez cargados los datos:
+# Mostrar el nombre del hombre con nota más baja
+# Mostrar el promedio de notas de las mujeres
+# Ejemplo:
+
+
+nombres = ["Juan","Pedro","Sol","Paco","Ana"]
+sexo = ["m","m","f","m","f"]
+nota = [6,8,10,8,5]
+
+nota_mas_baja = None
+cantidad_mujeres = 0
+suma_notas_mujeres = 0
+# nombres = []
+# sexos = []
+# notas = []
+# for _ in range(5):
+
+#     nombre = input('Nombre: ')
+#     while nombre == None or len(nombre) < 4:
+#         nombre = input('Nombre: ')
+#     nombres.append(nombre)
+
+#     sexo = input('Sexo: ')
+#     while sexo == None or sexo != "m" and sexo != "f":
+#         sexo = input('Sexo: ')
+#     sexos.append(sexo)
+
+#     nota = input('Nota: ')
+#     while nota.isdigit() == False or int(nota) < 0 or int(nota) > 10:
+#         nota = input('Nota: ')
+#     nota = int(nota)
+#     notas.append(nota)
+
+# for indice in range(len(nombres)):
+#     if nota_mas_baja == None or nota[indice] < nota_mas_baja:
+#         nombre_nota_mas_baja = nombres[indice]
+    
+#     if sexo[indice] == "f":
+#         cantidad_mujeres  += 1
+#         suma_notas_mujeres += nota[indice]
+
+# promedio_notas_mujeres = suma_notas_mujeres / cantidad_mujeres
+
+# print(f"{nombre_nota_mas_baja} es que tiene la nota mas baja\nEl promedio de la notas de las mujeres es {promedio_notas_mujeres}")
