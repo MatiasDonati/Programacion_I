b0ffb4ec1fa2d7542fb50dde0933959ff6b30b6c
MatiasDonati <matod87@hotmail.com>
2023-10-04 17:33:25 -0300

    stark 5

--- Desafios Starks/funciones_5.py
+++ Desafios Starks/funciones_5.py
@@ -7,17 +7,6 @@ from funciones_4 import *
 # MATIAS EDUARDO DONATI
 # DESAFIO STARK 5
 
-def leer_archivo(path:str)->list:
-    """Recibe un Json y devuelve una Lista"""
-
-    archivo = open(path, 'r')
-    dic_json = json.load(archivo)
-    archivo.close()
-    return dic_json['heroes']
-
-lista_heroes = leer_archivo('Desafios Starks/data_stark.json')
-# print(lista_heroes[0]["nombre"])
-
 def imprimir_menu_desafio_5():
     '''Imprime menu'''
     print("Menú de opciones:\n")
@@ -54,11 +43,8 @@ def stark_menu_principal_desafio_5():
 # print(stark_menu_principal_desafio_5())
 
 def stark_marvel_app_5(lista_heroes:list):
+   
 
-    # HACER DE DOS INCICOS UNA SOLA EJECUCION DE FUNCION CON IF
-    # HACER DE DOS INCICOS UNA SOLA EJECUCION DE FUNCION CON IF
-    # HACER DE DOS INCICOS UNA SOLA EJECUCION DE FUNCION CON IF
-    # HACER DE DOS INCICOS UNA SOLA EJECUCION DE FUNCION CON IF
     # HACER DE DOS INCICOS UNA SOLA EJECUCION DE FUNCION CON IF
 
    stark_normalizar_datos(lista_heroes)
@@ -69,6 +55,12 @@ def stark_marvel_app_5(lista_heroes:list):
 
     match opcion_usuario:
         case 'A':
+           
+            # ACA TENGO Q PASAR LISTA HEROES ME PARECE
+            # ACA TENGO Q PASAR LISTA HEROES ME PARECE
+            # ACA TENGO Q PASAR LISTA HEROES ME PARECE
+            # ACA TENGO Q PASAR LISTA HEROES ME PARECE
+           
             lista_masculino = obtener_dato_cantidad(lista_personajes, "M", "genero")
             stark_imprimir_heroes(lista_masculino)
         case 'B':
@@ -102,21 +94,100 @@ def stark_marvel_app_5(lista_heroes:list):
             if len(heroe_con_altura_maxima) == 1:
                 heroe = heroe_con_altura_maxima[0]
                 print(obtener_nombre(heroe))
-            pass
-        case 'K':
-            pass
+        case 'J' | 'K':
+            lista_ojos_pelo = []
+            clave = "color_ojos"
+            if opcion_usuario == 'K':
+                clave = "color_pelo"
+
+            for heroe in lista_personajes:
+                tipos_de_ojo_o_pelo = set(heroe[clave] for heroe in lista_personajes)
+
+            for tipo_ojo_o_pelo in tipos_de_ojo_o_pelo:
+                contador = 0
+                for heroe in lista_personajes:
+                    if tipo_ojo_o_pelo == heroe[clave]:
+                        contador += 1
+
+                diccionario_color_pelo_u_ojo = {}
+                diccionario_color_pelo_u_ojo[tipo_ojo_o_pelo] = contador
+
+                lista_ojos_pelo.append(diccionario_color_pelo_u_ojo)
+
+            stark_imprimir_heroes(lista_ojos_pelo)
         case 'L':
-            pass
-        case 'M':
-            pass
-        case 'N':
-            pass
-        case 'O':
-            pass
+            listado_por_inteligencia = []
+
+            for heroe in lista_personajes:
+                tipos_de_inteligencia = set(heroe["inteligencia"] for heroe in lista_personajes)
+
+            for tipo_de_inteligencia in tipos_de_inteligencia:
+                contador = 0
+                for heroe in lista_personajes:
+                    if tipo_de_inteligencia == heroe["inteligencia"]:
+                        contador += 1
+                               
+                diccionario_tipo_inteligencia = {}
+               
+                if tipo_de_inteligencia == "":
+                    diccionario_tipo_inteligencia["No tiene"] = contador
+                else:
+                    diccionario_tipo_inteligencia[tipo_de_inteligencia] = contador
+
+                listado_por_inteligencia.append(diccionario_tipo_inteligencia)
+
+            stark_imprimir_heroes(listado_por_inteligencia)
+        case 'M'| 'N'| 'O':
+           
+            clave = "color_ojos"
+            if opcion_usuario == "N":
+                clave = "color_pelo"
+            else:
+                clave = "inteligencia"
+
+            for heroe in lista_personajes:
+                tipo_ojo_o_inteligencia = set(heroe[clave] for heroe in lista_personajes)
+
+            for ojo_o_inteligencia in tipo_ojo_o_inteligencia:
+                print(f"# {ojo_o_inteligencia}")
+                print('')
+                for heroe in lista_personajes:
+                    if heroe[clave] == ojo_o_inteligencia:
+                        print(obtener_nombre_y_dato(heroe, clave))
+                        print('')
         case 'Z':
             print('Hasta luego!')
             break
         case -1:
             print(f"\nIngresó una opcion NO válida.\n")
 
+def leer_archivo(path:str)->list:
+    """Recibe un Json y devuelve una Lista"""
+
+    with open(path) as file:
+        data = json.load(file)      
+
+    # archivo = open(path, 'r')
+    # dic_json = json.load(archivo)
+    # archivo.close()
+    # return dic_json['heroes']
+
+    return data['heroes']
+       
+
+lista_heroes = leer_archivo('Desafios Starks/data_stark.json')
 stark_marvel_app_5(lista_heroes)
+
+
+# INSTARLAR RAINBOW VSC
+
+def guardar_archivo(nombre_archivo:str, contenido:str):
+    archivo = open(nombre_archivo, 'w+')
+    archivo.writelines(contenido)
+    archivo.close()
+    print(archivo.mode)
+    print(archivo.name)
+    print(archivo.closed)
+    # return archivo
+
+guardar_archivo("sarasa.csv", "ESTE ES EL CONTENIDO DEL ARasdasdasdasdasdCHIVO")
