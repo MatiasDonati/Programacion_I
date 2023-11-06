2c3481ccdb784d32e452e8bbc058130a967be9a0
MatiasDonati <matod87@hotmail.com>
2023-11-06 14:13:15 -0300

    frida

--- Frida/main.py
+++ Frida/main.py
@@ -27,7 +27,7 @@ pygame.time.set_timer(timer,100)
 
 
 #Creacion de Elementos
-player = Personaje(ANCHO_VENTANA/2,ALTO_VENTANA-ALTO_BRUJA,ANCHO_BRUJA, ALTO_BRUJA)
+frida = Personaje(ANCHO_VENTANA/2,ALTO_VENTANA-ALTO_BRUJA,ANCHO_BRUJA, ALTO_BRUJA)
 
 #Bloques
 bloque_uno = bloque.Bloque(ANCHO_VENTANA * 0.5/4, ALTO_VENTANA - ALTO_BRUJA * 1.1 - alto_bloque, ancho_bloque, alto_bloque)
@@ -64,42 +64,7 @@ while flag_run:
                 pass
 
     lista_teclas = pygame.key.get_pressed()
-    player.presionar_tecla(lista_teclas, bloques)
-
-    if not isJump:
-
-        if not sobre_bloque:
-            player.update(0 , -6 , "abajo")
-
-        if lista_teclas[pygame.K_SPACE]:
-            isJump = True
-    else:
-
-        if jumpCount >= -10:
-            velocidad = -(jumpCount * abs(jumpCount)) * 0.4
-            jumpCount -= 1
-        else:
-            jumpCount = 10
-            isJump = False
-        player.update( 0, - velocidad, "arriba")
-
-        #HACER QUE LA COLISION NO TENGA EFECTO SI TOCA LA CARA  DE LA BRUJA CON EL BLOQUE (si la bruja se choca con un objeto a la altura de su rectangulo)
-        #Q EL PISO SEA UN RECTANGULO DONDE COLISIONA!!!!
-        #Q EL PISO SEA UN RECTANGULO DONDE COLISIONA!!!!
-        for bloque_actual in bloques:
-            if player.rect_bruja.colliderect(bloque_actual.rect_bloque):
-                velocidad -= 10
-                jumpCount = 10
-                isJump = False
-                sobre_bloque = True
-                player.rect_bruja.y = (bloque_actual.rect_bloque.y - ALTO_BRUJA ) + ALTO_BRUJA * 1/7
-            else:
-                sobre_bloque = False
-
-    if player.rect_bruja.colliderect(bloque_uno.rect_bloque) == False and player.rect_bruja.colliderect(bloque_dos.rect_bloque) == False and player.rect_bruja.colliderect(bloque_tres.rect_bloque) == False and player.rect_bruja.colliderect(bloque_cuatro.rect_bloque) == False and player.rect_bruja.colliderect(bloque_cinco.rect_bloque) == False:
-          sobre_bloque = False
-    else:
-        sobre_bloque = True
+    frida.presionar_tecla(lista_teclas, bloques)
 
     #VOLCAR CAMBIOS
     ventana_ppal.fill(COLOR_FONDO)
@@ -107,7 +72,7 @@ while flag_run:
     fondo = pygame.transform.scale(fondo, (ANCHO_VENTANA, ALTO_VENTANA))
     ventana_ppal.blit(fondo, (0, 0))
 
-    player.actualizar_pantalla(ventana_ppal)
+    frida.actualizar_pantalla(ventana_ppal)
 
     for bloque_ in bloques:
         bloque_.actualizar_pantalla(ventana_ppal)
--- Frida/personaje.py
+++ Frida/personaje.py
@@ -7,19 +7,17 @@ ALTO_VENTANA = constantes.ALTO_VENTANA
 ANCHO_BRUJA = constantes.ANCHO_BRUJA
 ALTO_BRUJA = constantes.ALTO_BRUJA
 
-#Velocidades
+# Velocidades
 velocidad_base = 2
 velocidad_shift = 6
-
 
 class Personaje:
     def __init__(self, x, y, ancho, alto):
         self.surface = pygame.image.load("./imgs/bruja_color.png")
         self.surface = pygame.transform.scale(self.surface, (ancho, alto))
         self.rect_bruja = pygame.Rect(x, y, ancho, alto)
-        self.isJump = False  
-        self.jumpCount = 10 
-
+        self.isJump = False
+        self.jumpCount = 10
 
     def actualizar_pantalla(self, ventana_ppal):
         ventana_ppal.blit(self.surface, self.rect_bruja)
@@ -41,37 +39,33 @@ class Personaje:
 
         self.surface = pygame.transform.scale(self.surface, (ANCHO_BRUJA, ALTO_BRUJA))
 
-    
-    def presionar_tecla(self,lista_eventos, bloques):
+    def presionar_tecla(self, lista_eventos, bloques):
 
-        #Salto
+        # Salto
         sobre_bloque = False
 
         velocidad = velocidad_shift if lista_eventos[pygame.K_LSHIFT] else velocidad_base
 
         if lista_eventos[pygame.K_LEFT]:
-            self.update(-velocidad, 0 , "izquierda")
+            self.update(-velocidad, 0, "izquierda")
 
         if lista_eventos[pygame.K_RIGHT]:
-            self.update(velocidad , 0 , "derecha")
+            self.update(velocidad, 0, "derecha")
 
         if lista_eventos[pygame.K_DOWN]:
-            self.update(0 , -velocidad , "abajo")
+            self.update(0, -velocidad, "abajo")
 
-        #TRUCO VOLAR
         if lista_eventos[pygame.K_w]:
             if lista_eventos[pygame.K_UP]:
                 sobre_bloque = True
-                self.update(0 , velocidad , "arriba")
-        """
-         if not self.isJump:
+                self.update(0, 4, "arriba")
 
+        if not self.isJump:
             if not sobre_bloque:
-                self.update(0 , -6 , "abajo")
+                self.update(0, -6, "abajo")
 
             if lista_eventos[pygame.K_SPACE]:
                 self.isJump = True
-
         else:
             if self.jumpCount >= -10:
                 velocidad = -(self.jumpCount * abs(self.jumpCount)) * 0.4
@@ -79,26 +73,12 @@ class Personaje:
             else:
                 self.jumpCount = 10
                 self.isJump = False
-            self.update(0, - velocidad, "arriba")
+            self.update(0, -velocidad, "arriba")
 
-            #HACER QUE LA COLISION NO TENGA EFECTO SI TOCA LA CARA  DE LA BRUJA CON EL BLOQUE (si la bruja se choca con un objeto a la altura de su rectangulo)
-            #Q EL PISO SEA UN RECTANGULO DONDE COLISIONA!!!!
-            #Q EL PISO SEA UN RECTANGULO DONDE COLISIONA!!!!
-
-            for bloque_actual in bloques:
-                if self.rect_bruja.colliderect(bloque_actual.rect_bloque):
-                    print("colisionnnnnN")
-                    self.rect_bruja.y = bloque_actual.rect_bloque.y + bloque_actual.rect_bloque.height
-                    velocidad = 0  # Restablece la velocidad vertical
-                    self.update(0, 0, "abajo")
-                    sobre_bloque = True
-
-                else:
-                    sobre_bloque = False
-
-        if self.rect_bruja.colliderect(bloques[0].rect_bloque) == False and self.rect_bruja.colliderect(bloques[1].rect_bloque) == False and self.rect_bruja.colliderect(bloques[2].rect_bloque) == False and self.rect_bruja.colliderect(bloques[3].rect_bloque) == False and self.rect_bruja.colliderect(bloques[4].rect_bloque) == False:
-            sobre_bloque = False
-        else:
-            sobre_bloque = True
-        """
-       
+        for bloque_actual in bloques:
+            if self.rect_bruja.colliderect(bloque_actual.rect_bloque):
+                self.rect_bruja.y = bloque_actual.rect_bloque.y - (ALTO_BRUJA - ALTO_BRUJA * 1/7)
+                # sobre_bloque = False
+                self.isJump = False
+                if lista_eventos[pygame.K_SPACE]:
+                    self.isJump = True
